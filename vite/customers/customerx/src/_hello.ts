34eba3eaea90ba442f71ecc2a2b3b73538c64d77
mefjush <mateusz@adhese.eu>
2024-09-06 13:59:27 +0200

    Switch to the vite setup, update the docs (#18)

---
+++ vite/customers/customerx/src/_hello.ts
@@ -0,0 +1,5 @@
+// Multiple customers need this resource (with some minor customization) thus the `greet` function is imported from `common`.
+
+import { greet } from '../../../common/src/example/hello';
+
+greet("customerx");
---
+++ vite/customers/customerx/src/lib.ts
@@ -0,0 +1,3 @@
+// This is customerx-specific lib that (once published) will be available at `https://pool-customerx.adhese.com/tag/lib.js`
+
+console.log("Hello from a customerx-specific lib");
