8163d1db1029d8b5bed1d5ab25e336b73ffb4315
Wout De Rooms <wout.derooms@adhese.eu>
2024-10-18 10:36:07 +0200

    feat(core): improve dev and build workflows

--- vite/build.mjs
+++ vite/build.mjs
@@ -1,5 +1,5 @@
 import { build } from "vite";
-import { resolve } from 'path';
+import path, { resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { glob } from "glob";
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-let toFilename = (fullPath) => fullPath.replace(/^.*[\\/]/, '');
+let toFilename = (fullPath) => path.basename(fullPath);
 
 let env = process.env.env ? process.env.env : 'prod';
 let base = env == "prod" ? '/tag' : '/tag/test';
@@ -22,7 +22,7 @@ console.log(`Building for: ${customers}`);
 customers.forEach(async (customer) => {
 
   let commonBuildConfig = {
-    outDir: `../../dist/${customer}`,
+    outDir: path.join('..', '..', 'dist', customer),
     emptyOutDir: false,
     minify: 'terser',
     terserOptions: {
@@ -33,7 +33,7 @@ customers.forEach(async (customer) => {
   };
 
   // build htmls
-  let htmlFiles = glob.sync(resolve(__dirname, `customers/${customer}`, "*.html"));
+  let htmlFiles = glob.sync(resolve(__dirname, 'customers', customer, '*.html'));
 
   if (htmlFiles.length > 0) {
     await build({
@@ -41,7 +41,7 @@ customers.forEach(async (customer) => {
       base: base,
       plugins: [
         handlebars({
-          partialDirectory: resolve(__dirname, 'common/partials'),
+          partialDirectory: resolve(__dirname, 'common', 'partials'),
         }),
       ],
       build: {
@@ -53,12 +53,12 @@ customers.forEach(async (customer) => {
     });
   }
 
-  let libConfigs = glob.sync(resolve(__dirname, `customers/${customer}/src`, "*.ts"))
+  let libConfigs = glob.sync(resolve(__dirname, 'customers', customer, 'src', '*.ts'))
     .map(toFilename)
     .filter(fileName => !fileName.startsWith("_"))
     .map(fileName => ({
       entry: `src/${fileName}`,
-      name: fileName.replace(/\.[^/.]+$/, ""),
+      name: '[name].[ext]',
       formats: ['umd'],
       fileName: () => fileName.replace(".ts", ".js"),
     }));
@@ -66,7 +66,7 @@ customers.forEach(async (customer) => {
   // build libs
   libConfigs.forEach(async (libConfig) => {
     await build({
-      root: `customers/${customer}`,
+      root: path.join('customers', customer),
       base: base,
       build: {
         ...commonBuildConfig,
